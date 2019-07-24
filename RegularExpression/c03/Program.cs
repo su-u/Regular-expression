24372f5e04d609af4dd452d0a2dc942494b210c3
すー <aaaiiiuuu35@gmail.com>
2019-07-24 11:16:03 +0900

    任意の1文字

--- RegularExpression/c03/Program.cs
+++ RegularExpression/c03/Program.cs
@@ -1,9 +1,49 @@
-﻿namespace c03
+﻿using System;
+using System.ComponentModel.Design;
+using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+using static System.Console;
+
+namespace c03
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            //基本
+            WriteLine(RegexReplace( "That is a parasol", "a", "A", GetLine()));
+            
+
+            WriteLine(RegexReplace("Bob White", "Bob", "Rebert", GetLine()));
+            WriteLine(RegexReplace("Bobby White", "Bob", "Rebert", GetLine()));
+
+            WriteLine(RegexMatch("a..", "a..", GetLine()));
+            WriteLine(RegexMatch("a..", @"a\.\.", GetLine()));
+            WriteLine(RegexMatch("a..", "a\\.\\.", GetLine()));
+            WriteLine(RegexMatch("aaa", "a\\.\\.", GetLine()));
+            WriteLine(RegexMatch("aaa", "a..",GetLine()));
+            WriteLine(RegexMatch("aaa", @"a..",GetLine()));
+                        
+            //任意の1文字
+            WriteLine(RegexMatch( "aaa", @".", GetLine()));
+            WriteLine(RegexMatch( "abc", @"..", GetLine()));
+            WriteLine(RegexMatch( "   ", @"..", GetLine()));
+            WriteLine(RegexMatch( "  ", @".", GetLine()));
+
+        }
+
+        private static string RegexReplace(string input, string pattern, string replacement, int num = 0)
+        {
+            return ($"{num}: \"{input}\" => \"{pattern}\" -> \"{replacement}\" = \"{Regex.Replace(input, pattern, replacement)}\"");
+        }
+        private static string RegexMatch(string input, string pattern, int num = 0)
+        {
+            return ($"{num}: \"{input}\" -> \"{pattern}\" = \"{Regex.Match(input, pattern)}\"");
+        }
+        
+        private static int GetLine([CallerLineNumber]int line = 0)
+        {
+            return line;
         }
     }
 }
